e72a1b901f6c95a9f9980132c68e94f4a291e688
Ruban-exe <ruban.exe@gmail.com>
2023-01-26 22:57:19 +0300

    Even or Odd

--- venv/Python/Even_or_Odd.py
+++ venv/Python/Even_or_Odd.py
@@ -2,5 +2,6 @@
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 '''
 
+
 def even_or_odd(number):
     return 'Even' if number % 2 else 'Odd'
